301274f9bc5bfcdc23599bab114ef6bf071c0f9a
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-12-10 00:45:53 +0300

    Project has been created with Spring Initializr.

---
+++ src/main/java/com/bariser/financialguidebackend/FinancialGuideBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bariser.financialguidebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialGuideBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialGuideBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bariser/financialguidebackend/FinancialGuideBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bariser.financialguidebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialGuideBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
