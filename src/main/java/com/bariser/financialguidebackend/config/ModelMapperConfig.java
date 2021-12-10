6c9f3b01afbb8d51397908291277d91130be7f3f
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-12-10 23:03:56 +0300

    Modelmapper configuration has been added.

---
+++ src/main/java/com/bariser/financialguidebackend/config/ModelMapperConfig.java
@@ -0,0 +1,14 @@
+package com.bariser.financialguidebackend.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper getModelMapper(){
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
+}
